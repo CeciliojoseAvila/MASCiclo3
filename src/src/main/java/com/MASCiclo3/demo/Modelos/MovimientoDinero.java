4d958ec9f4169e6992117222d1e80ca4c82b88bb
Cecilio José Avila Ramos <110960338+CeciliojoseAvila@users.noreply.github.com>
2022-08-29 18:55:15 -0500

    creando entidad para los servicios, actualizacion y delete de datos.

--- src/src/main/java/com/MASCiclo3/demo/Modelos/MovimientoDinero.java
+++ src/src/main/java/com/MASCiclo3/demo/Modelos/MovimientoDinero.java
@@ -1,12 +1,59 @@
 package com.MASCiclo3.demo.Modelos;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Movimientos")
 public class MovimientoDinero {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    private String nombre;
-    private String dirección;
-    private String teléfono;
-    private String NIT;
+    private long monto;
+    private String dconcepto;
 
+    @ManyToOne
+    @JoinColumn(name = "Usuario_id")
+    private Empleado usuario;
 
+    public MovimientoDinero() {
+    }
+
+    public MovimientoDinero(long monto, String dconcepto, Empleado empleado) {
+        this.monto = monto;
+        this.dconcepto = dconcepto;
+        this.usuario = empleado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getMonto() {
+        return monto;
+    }
+
+    public void setMonto(long monto) {
+        this.monto = monto;
+    }
+
+    public String getDconcepto() {
+        return dconcepto;
+    }
+
+    public void setDconcepto(String dconcepto) {
+        this.dconcepto = dconcepto;
+    }
+
+    public Empleado getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Empleado usuario) {
+        this.usuario = usuario;
+    }
 }
---
+++ src/src/main/java/com/MASCiclo3/demo/Service/EmpresaService.java
@@ -0,0 +1,42 @@
+package com.MASCiclo3.demo.Service;
+
+import com.MASCiclo3.demo.Modelos.Empresa;
+import com.MASCiclo3.demo.Repositorios.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Le decimos a spring que la sgte anotacion es un servicio
+@Service
+public class EmpresaService {
+
+    @Autowired //Conectamos esta clase con el repositorio de Empresa
+    EmpresaRepository empresaRepository; //creamos un objeto de tipo EmpresaRepository para poder usar los metodos de dicha clase
+
+    //método que retornará la lista de empresas, usando metodos heredadis de JpaRepository
+   public List<Empresa> getAllEmpresas(){
+       List<Empresa> empresaList = new ArrayList<>();
+
+       empresaRepository.findAll().forEach(empresa -> empresaList.add(empresa)); //Recorremos el iterable que regresa el metodo finAll del jpa y lo guardamos en la lista
+
+       return empresaList;
+   }
+ // METODO QUE ME TRAE UN OBJETO DE TIPO EMPRESA CUANDO CUENTO CON EL ID DE LA MISMA
+   public Empresa getEmpresaById(Integer Id){
+
+    return empresaRepository.findById(Id).get();
+   }
+// METODO PARA ACTUALIZAR OBJETOS DE TIPO EMPRESA
+
+    public boolean saveOrUpdateEmpresa(Empresa empresa){
+       Empresa emp = empresaRepository.save(empresa);
+
+       if (empresaRepository.findById(emp.getId()) != null){
+
+           return true;
+       }
+        return false;
+    }
+}
