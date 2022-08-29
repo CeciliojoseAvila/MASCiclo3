701f47de5610c48c8d28dc8ababe8517f1ea9c95
Cecilio José Avila Ramos <110960338+CeciliojoseAvila@users.noreply.github.com>
2022-08-29 15:38:18 -0500

    creando entidad Empleado

---
+++ src/src/main/java/com/MASCiclo3/demo/Modelos/MovimientoDinero.java
@@ -0,0 +1,12 @@
+package com.MASCiclo3.demo.Modelos;
+
+public class MovimientoDinero {
+
+    private int id;
+    private String nombre;
+    private String dirección;
+    private String teléfono;
+    private String NIT;
+
+
+}
